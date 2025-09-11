662ef2e4afae3853175bf498c03201ae6c1eb057
Dilip Lohani <diliplohani5@gmail.com>
2025-09-11 22:37:37 +0530

    ReverseStringWords

--- src/questions/ReverseEachWordInAString.java
+++ src/questions/ReverseEachWordInAString.java
@@ -5,8 +5,25 @@ public class ReverseEachWordInAString {
 	public static void main(String[] args) {
 		String st = "Welcome to java";
 		
-		String[] wordStrings = st.split(" ");
+		String[] words = st.split(" ");
 		
+		String reveseString = "";
+		
+		for(String w:words) {
+			
+			String reverseW = "";
+			
+			for(int i=w.length()-1;i>=0;i--) {
+				
+				reverseW += w.charAt(i);
+			}
+			
+			reveseString += reverseW + " ";
+			
+		}
+		
+		
+		System.out.println(reveseString);
 
 	}
 
